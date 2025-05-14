f8b1e387faec2cb79ac3991c89a7c14deb8c522b
galactorsus( <504972@vistacollege.nl>
2025-05-14 12:56:27 +0200

    prototype 2
    functions:
    
    click to place tile
    
    places 6 tiles are specific locations once

---
+++ node_2d.gd
@@ -0,0 +1,23 @@
+extends Node2D
+@onready var tile_map_layer: TileMapLayer = $TileMapLayer
+
+
+func _ready():
+	pass
+	
+func _process(_delta):
+		pass
+
+func _input(event):
+	if Input.is_action_just_pressed("click"): 
+		print("fffff")
+		var grid_pos = tile_map_layer.local_to_map(get_local_mouse_position())
+		print(grid_pos)
+		tile_map_layer.set_cell(grid_pos, 0, Vector2i(0, 0), 0)
+		
+	tile_map_layer.set_cell(Vector2i(27,24), 0, Vector2i(1, 0), 0)
+	tile_map_layer.set_cell(Vector2i(27,25), 0, Vector2i(1, 0), 0)
+	tile_map_layer.set_cell(Vector2i(27,26), 0, Vector2i(1, 0), 0)
+	tile_map_layer.set_cell(Vector2i(29,24), 0, Vector2i(1, 1), 0)
+	tile_map_layer.set_cell(Vector2i(29,25), 0, Vector2i(1, 1), 0)
+	tile_map_layer.set_cell(Vector2i(29,26), 0, Vector2i(1, 1), 0)
