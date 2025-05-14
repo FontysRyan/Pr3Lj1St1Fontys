d4fb7f713ac5884e87978ec705be27862c38ad14
galactorsus( <504972@vistacollege.nl>
2025-05-14 14:53:35 +0200

    Update node_2d.gd

--- node_2d.gd
+++ node_2d.gd
@@ -2,37 +2,18 @@ extends Node2D
 @onready var tile_map_layer: TileMapLayer = $TileMapLayer
 var zones: int = 1
 var racks: int = 1
-var compartments: int = 5
+var compartments: int = 2
+var i: int = 0
 
 func _ready():
+	
 	#corner tile locations
-	tile_map_layer.set_cell(Vector2i(0,31), 0, Vector2i(2, 1), 0)
+
 	tile_map_layer.set_cell(Vector2i(71,0), 0, Vector2i(2, 1), 0)
 	tile_map_layer.set_cell(Vector2i(71,31), 0, Vector2i(2, 1), 0)
 	tile_map_layer.set_cell(Vector2i(0,0), 0, Vector2i(2, 1), 0)
-	#green line
-	tile_map_layer.set_cell(Vector2i(27,24), 0, Vector2i(1, 0), 0)
-	tile_map_layer.set_cell(Vector2i(27,25), 0, Vector2i(1, 0), 0)
-	tile_map_layer.set_cell(Vector2i(27,26), 0, Vector2i(1, 0), 0)
-	#purple line
-	tile_map_layer.set_cell(Vector2i(29,24), 0, Vector2i(1, 1), 0)
-	tile_map_layer.set_cell(Vector2i(29,25), 0, Vector2i(1, 1), 0)
-	tile_map_layer.set_cell(Vector2i(29,26), 0, Vector2i(1, 1), 0)
-
-	var i: int = 0
-
-	while i < 31:
-		print("i is", i)
-		tile_map_layer.set_cell(Vector2i(i,31), 0, Vector2i(2, 1), 0)
-		tile_map_layer.set_cell(Vector2i(i,0), 0, Vector2i(2, 1), 0)
-		var pos: int = 31 - i
-		tile_map_layer.set_cell(Vector2i(0,pos), 0, Vector2i(2, 1), 0)
-		i += 1
-	i = 0
-	while i < 28:
-		var pos2: int = 29 - i
-		tile_map_layer.set_cell(Vector2i(2,pos2), 0, Vector2i(0, 0), 0)
-		i += 1
+	
+	MakeZone()
 	
 func _process(_delta):
 		pass
@@ -44,3 +25,22 @@ func _input(event):
 		print(grid_pos)
 		tile_map_layer.set_cell(grid_pos, 0, Vector2i(0, 0), 0)
 		
+
+func MakeZone():
+	var zoneWidth: int = (5 * racks) 
+	var zoneHeight: int = (5 * compartments) 
+	print(zoneHeight)
+	print(zoneWidth)
+
+	for i in range(zoneHeight):
+			var Vwall = 31 - i
+			var Vwall2 = 4 * racks
+			tile_map_layer.set_cell(Vector2i(0,Vwall), 0, Vector2i(2, 1), 0) #start
+			tile_map_layer.set_cell(Vector2i(Vwall2,Vwall), 0, Vector2i(2, 1), 0) #start
+			i + 1
+	for i in range(zoneWidth):
+			var Hwall = 0 + i
+			var Hwall2 = 31 - (4 * compartments)
+			tile_map_layer.set_cell(Vector2i(Hwall,31), 0, Vector2i(2, 1), 0) #start
+			tile_map_layer.set_cell(Vector2i(Hwall,Hwall2), 0, Vector2i(2, 1), 0) #start
+			i + 1
